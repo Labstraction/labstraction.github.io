80d77b6d8970f92f71d087e67226abd9ce3b628b
Labstraction <45163263+Labstraction@users.noreply.github.com>
2019-02-05 13:06:45 +0100

    color update

--- labkanoid.js
+++ labkanoid.js
@@ -9,7 +9,7 @@ var dy = -2;
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#ffffff";
     ctx.fill();
     ctx.closePath();
 }
