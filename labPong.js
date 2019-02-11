6f4073793a0a4ecf18a0b172cf392b64c31ec225
Labstraction <45163263+Labstraction@users.noreply.github.com>
2019-02-11 01:34:14 +0100

    big commit

--- labPong.js
+++ labPong.js
@@ -1,16 +1,16 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 let ballRadius = 10;
-let x = canvas.width/2;
-let y = canvas.height-30;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 let paddleHeight = 75;
 let paddleWidth = 10;
 let paddle1x = 0;
-let paddle2x = canvas.width-paddleWidth;
-let paddle1y = (canvas.height-paddleHeight)/2;
-let paddle2y = (canvas.height-paddleHeight)/2;
+let paddle2x = canvas.width - paddleWidth;
+let paddle1y = (canvas.height - paddleHeight) / 2;
+let paddle2y = (canvas.height - paddleHeight) / 2;
 let upPressed = false;
 let downPressed = false;
 
@@ -18,59 +18,59 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
-    if(e.key == "Up" || e.key == "ArrowUp") {
+    if (e.key == "Up" || e.key == "ArrowUp") {
         upPressed = true;
     }
-    else if(e.key == "Down" || e.key == "ArrowDown") {
+    else if (e.key == "Down" || e.key == "ArrowDown") {
         downPressed = true;
     }
 }
 
 function keyUpHandler(e) {
-    if(e.key == "Up" || e.key == "ArrowUp") {
+    if (e.key == "Up" || e.key == "ArrowUp") {
         upPressed = false;
     }
-    else if(e.key == "Down" || e.key == "ArrowDown") {
+    else if (e.key == "Down" || e.key == "ArrowDown") {
         downPressed = false;
     }
 }
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#ffffff";
     ctx.fill();
     ctx.closePath();
 }
-function drawPaddle(x,y) {
+function drawPaddle(x, y) {
     ctx.beginPath();
     ctx.rect(x, y, paddleWidth, paddleHeight);
     ctx.fillStyle = "#ffffff";
     ctx.fill();
     ctx.closePath();
-    
+
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    drawPaddle(paddle1x,paddle1y);
-    drawPaddle(paddle2x,paddle2y);
-    
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    drawPaddle(paddle1x, paddle1y);
+    drawPaddle(paddle2x, paddle2y);
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
-    
-    if(upPressed && paddle2y > 0) {
+
+    if (upPressed && paddle2y > 0) {
         paddle2y -= 4;
     }
-    else if(downPressed && paddle2y < canvas.height-paddleHeight) {
+    else if (downPressed && paddle2y < canvas.height - paddleHeight) {
         paddle2y += 4;
     }
-    
+
     x += dx;
     y += dy;
 }
