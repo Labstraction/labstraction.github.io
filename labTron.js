c96fb28c3cc80a48aa17df696aa743ec804ff119
Labstraction <45163263+Labstraction@users.noreply.github.com>
2019-02-18 16:07:24 +0100

    tron2

--- labTron.js
+++ labTron.js
@@ -1,7 +1,45 @@
+
+
+
+
+let behaviours = (function() {
+
+    function randomBehaviour(players){
+        players.forEach(player => {
+            
+        });
+    }
+
+    function trackBehaviour(players){
+        players.forEach(player => {
+            
+        });
+    }
+
+    function goAwayBeaviour(players){
+        players.forEach(player => {
+            
+        });
+    };
+
+    let allBeahaviours = [randomBehaviour,trackBehaviour,goAwayBeaviour];
+
+    return{
+        randomBehaviour:randomBehaviour,
+        trackBehaviour:trackBehaviour,
+        goAwayBeaviour:goAwayBeaviour,
+        allBeahaviours:allBeahaviours
+    }
+
+})();
+
+
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-const START_Y = canvas.height-1;
-const colors = ["red", "yellow", "blue", "green"];
+const START_Y = Math.trunc(canvas.height-1);
+const COLORS = ["red", "green", "purple"];
+const TAIL_LENGHT = 800;
+const BLACK = "#000000";
 let players = [];
 
 
@@ -13,14 +51,16 @@ function init(){
     ctx.strokeStyle= "#ffffff";
     ctx.rect(0,0, canvas.width, canvas.height);
     ctx.stroke();
-    for (let index = 0; index < colors.length; index++) {
+    for (let index = 0; index < COLORS.length; index++) {
         
         let player = {
-            actualX : ((canvas.width-2)/(colors.length+1))*(1+index),
+            actualX : Math.trunc(((canvas.width-2)/(COLORS.length+1))*(1+index)),
             actualY : START_Y,
             horizontalDirection : 0,
             verticalDirection : -1,
-            color: colors[index]
+            color: COLORS[index],
+            points:[]
+            
         }
 
         players.push(player);
@@ -32,16 +72,12 @@ function init(){
 
 
 function checkWall(player){
+
+    console.log(player);
     
     let nextMoveX = player.actualX+(player.horizontalDirection*3);
     let nextMoveY = player.actualY+(player.verticalDirection*3);
 
-    if (nextMoveX<0||nextMoveX>canvas.width) {
-        return false;
-    }
-    if (nextMoveY<0||nextMoveY>canvas.height) {
-        return false;
-    }
 
     let imageData = ctx.getImageData(nextMoveX, nextMoveY, 1, 1).data;
     return imageData[0]+imageData[1]+imageData[2]!=0;
@@ -51,32 +87,48 @@ function controlPlayerMovment(player){
     if(checkWall(player)){
         player.horizontalDirection = player.horizontalDirection == 0? (Math.random() < 0.5 ? -1 : 1) : 0;
         player.verticalDirection = player.verticalDirection == 0? (Math.random() < 0.5 ? -1 : 1) : 0;
-        controlPlayerMovment();
+        controlPlayerMovment(player);
+    }else{
+        player.actualX += player.horizontalDirection;
+        player.actualY += player.verticalDirection;
+        point={
+            x:player.actualX,
+            y:player.actualY,
+        }
+        player.points.push(point);
+        if(player.points.length>TAIL_LENGHT){
+            let point = player.points.shift();
+            drawPoint(point.x, point.y, BLACK);
+        }
     }
 
-    player.actualX += player.horizontalDirection;
-    player.actualY += player.verticalDirection;
+    
 }
 
+function drawPlayer(player){
+    drawPoint(player.actualX,player.actualY,player.color);
+}
 
-function drawPlayer(player) {
+function drawPoint(x,y,color){
     ctx.beginPath();
-    ctx.fillStyle = player.color;
-    ctx.fillRect(player.actualX, player.actualY, 1, 1);
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, 1, 1);
+    ctx.closePath();
 }
 
 
 
 function draw() {
-    players.forEach(player => {
+
+        players.forEach(player => {
         controlPlayerMovment(player);
         drawPlayer(player);
     });
-    
-
   
 }
 
 init();
 
-setInterval(draw, 2);
+setInterval(draw, 1);
+
+
